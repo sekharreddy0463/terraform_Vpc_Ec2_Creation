98198a3cf19796087c6c356e730942af6f891c95
sekharreddy0463 <53187022+sekharreddy0463@users.noreply.github.com>
2024-03-01 21:17:49 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,7 @@
+# Authentication
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIAXZKY5NATVNJRFYWG"
+  secret_key = "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"
+}
+data "aws_availability_zones" "available" {}
